e1add17f10f0fe0d458ee584d0e539edfe089589
Peter Collins <onepetercollins@gmail.com>
2026-05-07 09:44:28 +0100

    T-17a: SectionBase component
    - 3-prop slot wrapper (label, labelledby, constrain)
    - constrain=true wraps slot in max-w-[1920px] mx-auto div
    - labelledby takes precedence over label; aria-label suppressed when labelledby set
    - No style block; inheritAttrs defaults to true for class passthrough

---
+++ components/SectionBase.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  label:      { type: String,  default: undefined },
+  labelledby: { type: String,  default: undefined },
+  constrain:  { type: Boolean, default: true }
+})
+</script>
+
+<template>
+  <section
+    :aria-label="labelledby ? undefined : (label || undefined)"
+    :aria-labelledby="labelledby || undefined"
+  >
+    <div v-if="constrain" class="max-w-[1920px] mx-auto">
+      <slot />
+    </div>
+    <slot v-else />
+  </section>
+</template>
